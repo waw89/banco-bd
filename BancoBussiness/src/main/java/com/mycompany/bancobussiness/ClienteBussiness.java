6a1f9d3be508ecceda6aa12f79ee989ca6e7778e
waw <lcontrerasperaza99@gmail.com>
2024-02-27 16:21:29 -0700

    Crear cliente.

---
+++ BancoBussiness/src/main/java/com/mycompany/bancobussiness/ClienteBussiness.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bancobussiness;
+
+import com.mycompany.bancodomain.Cliente;
+
+/**
+ *
+ * @author luisp
+ */
+public class ClienteBussiness {
+    
+    
+    /**
+     * Metodo para crear un nuevo cliente
+     */
+    
+    public Cliente crear(Cliente cliente){
+        
+        
+        
+    }
+}
--- BancoGUI/src/main/java/com/mycompany/bancogui/BancoGUI.java
+++ BancoGUI/src/main/java/com/mycompany/bancogui/BancoGUI.java
@@ -4,6 +4,14 @@
 
 package com.mycompany.bancogui;
 
+import com.mycompany.bancobussiness.ClienteBussiness;
+import com.mycompany.bancodomain.Cliente;
+import com.mycompany.bancodomain.Domicilio;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import javax.xml.crypto.Data;
+
 /**
  *
  * @author luisp
@@ -11,6 +19,19 @@ package com.mycompany.bancogui;
 public class BancoGUI {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        /**
+         * 
+         * A continuación el código de prueba para crear una cuenta
+         * 
+         */
+        Domicilio domicilio = new Domicilio("Valle Escondido", "Entre valle triste y valle del sol", 85090, "Miravalle", "CASA BLANCA");
+        Cliente cliente = new Cliente("Jesus", "Contreras", domicilio, 0, new Date(103, 5, 7), "COPL030607HSRNRSA9");
+        
+        
+        ClienteBussiness clienteBs = new ClienteBussiness(); 
+        
+        clienteBs.crear(cliente);
+        
     }
 }
