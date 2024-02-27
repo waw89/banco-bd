b54f5cdccc84a69a2a08e1e9b93aeffd42a882ae
waw <lcontrerasperaza99@gmail.com>
2024-02-27 16:33:33 -0700

    MySQL

---
+++ BancoPersistence/src/main/java/com/mycompany/bancopersistence/ConnectionMYSQL.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bancopersistence;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author luisp
+ */
+public class ConnectionMYSQL {
+
+    public void createConnection() {
+
+    }
+}
