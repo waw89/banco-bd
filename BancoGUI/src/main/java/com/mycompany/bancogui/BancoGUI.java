2697ff956eadbbd627f74c11309ea3e1c0f5af7a
waw <lcontrerasperaza99@gmail.com>
2024-02-27 16:00:50 -0700

    Creación de proyectos para GUI, Persistencia, Negocio.

---
+++ BancoGUI/src/main/java/com/mycompany/bancogui/BancoGUI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bancogui;
+
+/**
+ *
+ * @author luisp
+ */
+public class BancoGUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
